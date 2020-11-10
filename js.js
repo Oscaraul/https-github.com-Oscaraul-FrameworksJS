6a2bcabeeefbc35b1c106ebb7be285c52986d07c
Oscaraul <racsoluar_391@hotmail.com>
2020-11-09 20:26:10 -0500

    line

--- js.js
+++ js.js
@@ -1,20 +1,7 @@
-var contacts = ['Chris:2232322', 'Sarah:3453456', 'Bill:7654322', 'Mary:9998769', 'Dianne:9384975'];
-var para = document.querySelector('p');
-var input=document.querySelector('input');
-var btn = document.querySelector('button');
+var canvas=document.getElementById('miCanva'),
+context= canvas.getContext('2d');
 
-
-btn.addEventListener('click',function(){
-  var searchName = input.value;
-  input.value="";
-
-  for(var i=0; i<contacts.length;i++){
-    var splitContact = contacts[i].split(':');
-    if(splitContact[0]===searchName){
-      para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
-      break;
-    }else {
-      para.textContent = 'Contact not found.';
-    }
-  }
-});
+context.beginPath();
+context.moveTo(100,30);
+context.lineTo(200,150);
+context.stroke();
