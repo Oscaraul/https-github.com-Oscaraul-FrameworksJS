f25764ca7bbb5447e498253b600706a670c68951
Oscaraul <racsoluar_391@hotmail.com>
2020-11-11 06:57:03 -0500

    Texto

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 var canvas=document.getElementById('miCanvas'),
 context= canvas.getContext('2d');
 
-context.arc(120, 120, 70, 0, 2*Math.PI,false);
-context.stroke();
+context.font='bold 30pt Arial, san-serif';
+context.fillText('Oscar Raúl Rengifo López', 20, 100,);
