e76de7b2346ca6d4c849bdfc8b1615e80512a6b5
Oscaraul <racsoluar_391@hotmail.com>
2020-11-10 19:55:07 -0500

    moveTO

--- js.js
+++ js.js
@@ -1,7 +1,24 @@
-var canvas=document.getElementById('miCanva'),
+var canvas=document.getElementById('miCanvas'),
 context= canvas.getContext('2d');
 
 context.beginPath();
-context.moveTo(100,30);
-context.lineTo(200,150);
+
+context.moveTo(30,300);
+context.lineTo(30,100);
+
+context.moveTo(30,300);
+context.lineTo(200,300);
+
+context.moveTo(200,300);
+context.lineTo(200,100);
+
+context.moveTo(30,100);
+context.lineTo(200,100);
+
+context.moveTo(30,300);
+context.lineTo(200,100);
+
+context.moveTo(500,100);
+context.lineTo(400,100);
+
 context.stroke();
