64c100741d446fdb1339d8ce8d1fd8acf5dcbccd
Oscaraul <racsoluar_391@hotmail.com>
2020-11-10 20:04:46 -0500

    Arco

--- js.js
+++ js.js
@@ -1,12 +1,5 @@
 var canvas=document.getElementById('miCanvas'),
 context= canvas.getContext('2d');
 
-context.beginPath();
-
-context.rect(50,100,100,50);
-context.fillStyle="Blue";
-context.fill();
-context.lineWidth = 5;
-context.strokeStyle= "black";
-
+context.arc(100,100,50,Math.PI,Math.PI*1.5,false);
 context.stroke();
