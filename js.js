b323ca544172f33272bb319f6a721a38e9bb684e
Oscaraul <racsoluar.391@gmail.com>
2020-11-13 07:05:54 -0500

    Konva

--- js.js
+++ js.js
@@ -1,9 +1,6 @@
 var canvas=document.getElementById('miCanvas'),
 context= canvas.getContext('2d');
 
-var imageObj=new Image();
-imageObj.onload=function () {
-  context.drawImage(imageObj, 50, 50, 500, 350);
-};
 
-imageObj.src='lenovo.png'
+context.arc(100, 100, 50, Math.PI, Math.PI*1,5, false);
+context.stroke();
