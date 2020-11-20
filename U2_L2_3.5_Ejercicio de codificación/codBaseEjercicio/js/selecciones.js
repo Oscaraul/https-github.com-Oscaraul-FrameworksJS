309cd56366fb18e5493475dad3d0a30e489a97c0
Oscaraul <racsoluar.391@gmail.com>
2020-11-20 06:55:23 -0500

    EstilosJquery

--- U2_L2_3.5_Ejercicio de codificación/codBaseEjercicio/js/selecciones.js
+++ U2_L2_3.5_Ejercicio de codificación/codBaseEjercicio/js/selecciones.js
@@ -1,8 +1,12 @@
 $( document ).ready(function() {
   $("body").click(function(){
     //Realiza las selecciones en este bloque de código
+    $("div > h1").css("color","yellow");
+    $("div > a").find("i").css("color","yellow");
+    $("#fh5co-main-nav").find(":contains('Home')").css("color","yellow");
+    $("img[src$='2.jpg']+a").find(":contains('15 images')").css("color","yellow");
 
+    $("div[data-section='about'] div:nth-child(2)").find("h2:contains('Jean Smith') ~ p").css("color","yellow");
 
-    
   });
 });
